02449a39ec0ad55043c101694cc97ac6eba8e858
cursoskanechan <kanes.chan@gmail.com>
2022-06-20 08:45:21 -0300

    feat: Aplicado styles aos componentes.

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,11 @@ const App = () => {
           <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
-        ...
+        {/* Área de informações gerais */}
+
+        {/* Área de cadastro */}
+
+        {/* Relação de cadastros */}
       </C.Body>
     </C.Container>
   );
