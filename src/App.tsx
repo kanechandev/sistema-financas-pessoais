66999773b9283725211c91f7c7d9cc93895e6a1c
cursoskanechan <kanes.chan@gmail.com>
2022-06-20 10:18:20 -0300

    feat: Add helpers de mês atual e listagem por mês.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { useState, useEffect } from 'react';
 import * as C from './App.styles';
+import { Categoria } from './types/Categoria';
+import { Item } from './types/Item';
+import { categorias } from './data/categorias';
+import { items } from './data/items';
+import { filterListByMonth, getCurrentMonth } from './helpers/dateFilter';
 
 const App = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth));
+  }, [list, currentMonth]);
+
   return (
     <C.Container>
       <C.Header>
