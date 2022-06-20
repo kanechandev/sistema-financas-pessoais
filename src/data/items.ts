9a0ee9d8abaf396d16a2cae8cb41af845c24a152
cursoskanechan <kanes.chan@gmail.com>
2022-06-20 09:23:52 -0300

    feat: Add model Item e dados para popular.

---
+++ src/data/items.ts
@@ -0,0 +1,9 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { data: new Date(2022, 3, 22), categoria: 'Alimentação',  titulo: 'Subway', valor: 29.95 },
+    { data: new Date(2022, 3, 15), categoria: 'Alimentação',  titulo: 'Sorvete', valor: 6.50 },
+    { data: new Date(2022, 3, 12), categoria: 'Conta',  titulo: 'Luz', valor: 199.95 },
+    { data: new Date(2022, 3, 21), categoria: 'Conta',  titulo: 'Água', valor: 79.95 },
+    { data: new Date(2022, 3, 21), categoria: 'Salário',  titulo: 'XYZ Ltda.', valor: 2300.54 },
+];
---
+++ src/types/Item.ts
@@ -0,0 +1,6 @@
+export type Item = {
+    data: Date;
+    categoria: string;
+    titulo: string;
+    valor: number;
+}
